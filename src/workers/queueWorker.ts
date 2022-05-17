3a491f52d9529df5ced6fd1495f89175d30e061d
Samundra Khatri <samundra.khatri@joinsage.com>
2022-05-17 16:07:28 +0545

    add more workers

--- src/workers/queueWorker.ts
+++ src/workers/queueWorker.ts
@@ -37,7 +37,7 @@ export default function (queue: Queue, em: EntityManager) {
     },
     {
       connection: redisConnection,
-      concurrency: 20,
+      concurrency: 10,
     }
   );
 
@@ -60,10 +60,67 @@ export default function (queue: Queue, em: EntityManager) {
     if (job.name === JobTypeEnum.LOCAL_CENTRE) {
       df = new DataFetchStrategy(new ElectionCentreStrategy());
     }
-    if (!(job.name === JobTypeEnum.STATE && !returnValue.next)) {
-      await df.save(em, job.data);
+
+    await df.save(em, job.data);
+    if (!returnValue.next) return;
+    returnValue.payload.value.forEach((district) => {
+      queue.add(returnValue.next, {
+        value: district,
+        parent: returnValue.payload.parent,
+      });
+    });
+  });
+
+  const worker1 = new Worker(
+    queueName,
+    async (job: Job<IStandardData>) => {
+      let df: DataFetchStrategy;
+      if (job.name === JobTypeEnum.STATE) {
+        df = new DataFetchStrategy(new StateStrategy());
+      }
+      if (job.name === JobTypeEnum.DISTRICT) {
+        df = new DataFetchStrategy(new DistrictStrategy());
+      }
+
+      if (job.name === JobTypeEnum.MUNCIPAL) {
+        df = new DataFetchStrategy(new MunicipalStrategy());
+      }
+
+      if (job.name === JobTypeEnum.WARD) {
+        df = new DataFetchStrategy(new WardStrategy());
+      }
+      if (job.name === JobTypeEnum.LOCAL_CENTRE) {
+        df = new DataFetchStrategy(new ElectionCentreStrategy());
+      }
+      return await df.fetch(em, job.data);
+    },
+    {
+      connection: redisConnection,
+      concurrency: 10,
+    }
+  );
+
+  worker1.on("completed", async (job, returnValue: IDataFetchResponse) => {
+    let df: DataFetchStrategy;
+    if (job.name === JobTypeEnum.STATE) {
+      df = new DataFetchStrategy(new StateStrategy());
+    }
+    if (job.name === JobTypeEnum.DISTRICT) {
+      df = new DataFetchStrategy(new DistrictStrategy());
     }
 
+    if (job.name === JobTypeEnum.MUNCIPAL) {
+      df = new DataFetchStrategy(new MunicipalStrategy());
+    }
+
+    if (job.name === JobTypeEnum.WARD) {
+      df = new DataFetchStrategy(new WardStrategy());
+    }
+    if (job.name === JobTypeEnum.LOCAL_CENTRE) {
+      df = new DataFetchStrategy(new ElectionCentreStrategy());
+    }
+
+    await df.save(em, job.data);
     if (!returnValue.next) return;
     returnValue.payload.value.forEach((district) => {
       queue.add(returnValue.next, {
